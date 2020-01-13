b86222beb98ba0b4094d764345f7e68c95051ca2
Larry Fritts <larry.fritts@linuxacademy.com>
2020-01-13 12:03:02 -0800

    added snippet to get notebook token and a folder to hold data files

---
+++ snippets/get_notebook_token.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+
+# start jupyter notebook server on port 8086
+subprocess.Popen(
+                 ['nohup', 'jupyter', 'notebook', '--no-browser', '--port=8086'],
+                 stdout=open('/dev/null', 'w'),
+                 stderr=open('logfile.log', 'a'),
+                 preexec_fn=os.setpgrp)
+
+# write jupyter server token to screen
+token_file = open("nohup.out")
+
+for line in token_file:
+    if line.find("token=") > 0:
+        print(line[line.find("token=")+6: ])
+        break
+
+token_file.close()
+
+
+
+
+
+
