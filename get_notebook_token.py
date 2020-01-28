4a96be57a2a519d28c85127730d1a405aadf6599
Larry Fritts <larry.fritts@linuxacademy.com>
2020-01-28 12:51:00 -0800

    not sure why these are not there

--- get_notebook_token.py
+++ get_notebook_token.py
@@ -1,22 +1,3 @@
-# from os import setpgrp
-# from subprocess import Popen, PIPE
-
-# p = Popen(
-#         ['nohup', 'jupyter', 'notebook', '--no-browser', '--port=8086'],
-#         stdout=PIPE,stderr=PIPE, preexec_fn=setpgrp)
-
-# out, err = p.communicate()
-# print ("cat returned code = %d" % p.returncode)
-# print ("cat output:\n\n%s\n\n" % out)
-# print ("cat errors:\n\n%s\n\n" % err)
-
-# # token_file = open("logfile.log")
-
-# # for line in token_file:
-# #     if line.find("token=") > 0:
-# #         print(line[line.find("token=")+6: ])
-# #         break
-
 import subprocess
 import os
 import time
